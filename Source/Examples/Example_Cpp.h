6d36f545d56964e7588dc6180b44e17dbfef9207
MaximumOctopus <40474413+MaximumOctopus@users.noreply.github.com>
2023-11-27 13:43:32 +0000

    First commit

---
+++ Source/Examples/Example_Cpp.h
@@ -0,0 +1,23 @@
+// ===================================================================
+//
+//   (c) Paul Alan Freshney 2012-2023
+//   www.freshney.org :: paul@freshney.org :: maximumoctopus.com
+//
+//   https://github.com/MaximumOctopus/LEDMatrixStudio
+//
+//   https://maximumoctopus.hashnode.dev/
+//
+//   C++ Rewrite October 11th 2023
+//
+// ===================================================================
+
+#pragma once
+
+
+namespace ExampleCPP
+{
+	std::wstring Get()
+	{
+		return L"";
+	}
+
---
+++ Source/Examples/Example_FastLED.h
@@ -0,0 +1,44 @@
+// ===================================================================
+//
+//   (c) Paul Alan Freshney 2012-2023
+//   www.freshney.org :: paul@freshney.org :: maximumoctopus.com
+//
+//   https://github.com/MaximumOctopus/LEDMatrixStudio
+//
+//   https://maximumoctopus.hashnode.dev/
+//
+//   C++ Rewrite October 11th 2023
+//
+// ===================================================================
+
+#pragma once
+
+
+namespace ExampleFastLED
+{
+	std::wstring Get(int start, int end, int pixelsperframe)
+	{
+		std::wstring s = L"";
+
+		s += L"const int __NUM_LEDS = ' + IntToStr(aPixelsPerFrame) + '; // per frame\n";
+		s += L"const unsigned long __DELAY_MS = 225;\n";
+		s += L"\n";
+		s += L"void loop() {\n";
+		s += L"\n";
+
+		for (int t = start; t <= end; t++)
+		{
+			s += L"  FastLED.clear();\n";
+			s += L"  for(int i = 0; i < __NUM_LEDS; i++)\n";
+			s += L"  {\n";
+			s += L"    leds[i] = pgm_read_dword(&(ledarray' + IntToStr(t - 1) + '[i]));\n";
+			s += L"  }\n";
+			s += L"  FastLED.show();\n";
+			s += L"  delay(__DELAY_MS);\n";
+			s += L"\n";
+		}
+
+		return s += L"}\n";
+	}
+}
+
